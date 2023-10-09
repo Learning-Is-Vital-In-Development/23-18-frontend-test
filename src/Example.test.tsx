d5df8c448aa371b5738d36a4f8d67b7e23a9fb68
Lee Dogyeong <shroad1802@gmail.com>
2023-10-09 22:37:11 +0900

    Update Example.test.tsx

--- src/Example.test.tsx
+++ src/Example.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { Example } from './Example';
 
 describe('Example', () => {
-  it('should render without crashing', () => {
+  it('should render without crash', () => {
     render(<Example />);
 
     expect(screen.getByText('Hello, World!')).toBeInTheDocument();
