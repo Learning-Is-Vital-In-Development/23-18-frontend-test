96f9ebc172f2371b04ae9129cc4b6618b65de3b4
dogyeong <shroad1802@gmail.com>
2023-11-21 18:08:25 +0900

    test

--- src/FetchExample.test.tsx
+++ src/FetchExample.test.tsx
@@ -4,6 +4,10 @@ import { render, screen } from '@testing-library/react';
 import { FetchExample } from './FetchExample';
 
 describe('FetchExample', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('api 응답에 있는 메뉴 목록 타이틀이 화면에 보여야 한다.', async () => {
     render(<FetchExample />);
 
@@ -28,4 +32,34 @@ describe('FetchExample', () => {
 
     expect(await screen.findByText('맛있는 토핑')).toBeInTheDocument();
   });
+
+  it.each`
+    hour    | minute  | expected
+    ${'09'} | ${'59'} | ${'영업종료'}
+    ${'10'} | ${'00'} | ${'영업중'}
+    ${'10'} | ${'01'} | ${'영업중'}
+    ${'21'} | ${'59'} | ${'영업중'}
+    ${'22'} | ${'00'} | ${'영업종료'}
+    ${'22'} | ${'01'} | ${'영업종료'}
+  `(
+    '영업시간이 10~22시고, 현재 시간이 $hour시 $minute분이라면, $expected이/가 화면에 보여야 한다.',
+    async ({ hour, minute, expected }: { hour: string; minute: string; expected: string }) => {
+      server.use(
+        http.get('/api/store/:storeId', () => {
+          return HttpResponse.json({
+            openHour: 10,
+            closeHour: 22,
+            storeMenu: [],
+          });
+        }),
+      );
+
+      const now = new Date(`2023-01-01T${hour}:${minute}:00`).getTime();
+      jest.spyOn(global.Date, 'now').mockImplementation(() => now);
+
+      render(<FetchExample />);
+
+      expect(await screen.findByText(expected)).toBeInTheDocument();
+    },
+  );
 });
--- src/FetchExample.tsx
+++ src/FetchExample.tsx
@@ -3,15 +3,33 @@ import { type MenuList } from 'src/server/data';
 
 export const FetchExample = () => {
   const [storeMenu, setStoreMenu] = useState<MenuList[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    void fetch(`${location.href}api/store/1234`)
-      .then((response) => response.json())
-      .then(({ storeMenu }) => setStoreMenu(storeMenu as MenuList[]));
+    void (async () => {
+      const response = await fetch(`${location.href}api/store/1234`);
+      const { openHour, closeHour, storeMenu } = (await response.json()) as {
+        openHour: number;
+        closeHour: number;
+        storeMenu: MenuList[];
+      };
+
+      setStoreMenu(storeMenu);
+
+      const hour = new Date(Date.now()).getHours();
+      setIsOpen(hour >= openHour && hour < closeHour);
+      setIsLoading(false);
+    })();
   }, []);
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <>
+      {isOpen ? <div>영업중</div> : <div>영업종료</div>}
       {storeMenu.map(({ id, title }) => (
         <div key={id}>{title}</div>
       ))}
